68c33f46ecb78b32867a1975bfd60e549ac7a5e3
Dang Ba Do <dodb@techasians.com>
2022-06-10 09:15:19 +0700

    add new config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      // custom width
       width: {
         '536': '536px',
         '980': '980px',
@@ -10,21 +11,27 @@ module.exports = {
         "432": '432px',
         '170': '170px'
       },
+      // custom box-shadow
       boxShadow: {
         '3xl': '0px 0px 15px 3px rgba(0,0,0,0.75)',
         "4xl": '0px 0px 41px -15px rgba(0,0,0,0.62)',
         'register': '0px 0px 12px 4px rgba(0,0,0,0.09)'
       },
+      // custom top-bottom-left-right
       inset: {
         '14px': '14px'
       },
+      // custom padding,margin
       spacing: {
         '18px': '18px',
         '10px': '10px',
         '11px': '11px'
       },
+      // custom color
       colors: {
-        'red-be8': '#ffebe8'
+        'red-be8': '#ffebe8',
+        '770': '#606770',
+        'e21': '#1c1e21'
       },
       fontSize: {
         '15px': '15px',
@@ -32,10 +39,6 @@ module.exports = {
       },
       lineHeight: {
         '48px': '48px'
-      },
-      colors: {
-        '770': '#606770',
-        'e21': '#1c1e21'
       }
     },
   },
