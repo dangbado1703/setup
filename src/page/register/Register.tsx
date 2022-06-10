a69d741663aeb60aa76ca600f9e31a54226db2d9
Dang Ba Do <dodb@techasians.com>
2022-06-10 12:03:37 +0700

    add register

--- src/page/register/Register.tsx
+++ src/page/register/Register.tsx
@@ -1,7 +1,7 @@
 import { faCircleExclamation, faCircleQuestion } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useEffect, useState } from 'react';
-import { useForm } from 'react-hook-form';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import { range } from '../../config/constants/constants';
 
 interface FormRegister {
@@ -29,7 +29,18 @@ const Register: React.FC = () => {
     register,
     formState: { errors },
     handleSubmit,
-  } = useForm<FormRegister>();
+  } = useForm<FormRegister>({
+    defaultValues: {
+      last_name: '',
+      first_name: '',
+      username: '',
+      password: '',
+      day: new Date().getDate().toString(),
+      month: (new Date().getMonth() + 1).toString(),
+      year: new Date().getFullYear().toString(),
+      gen: '',
+    },
+  });
   useEffect(() => {
     setValue({
       day: new Date().getDate(),
@@ -38,38 +49,65 @@ const Register: React.FC = () => {
     });
   }, []);
 
+  const submitForm: SubmitHandler<FormRegister> = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="w-432 bg-white rounded-xl shadow-register">
-        <form>
+        <form onSubmit={handleSubmit(submitForm)}>
           <div className="px-4 py-10px text-center border-b">
             <h2 className="text-2xl text-e21 font-semibold leading-8">Tạo tài khoản mới</h2>
             <p className="text-15px leading-6 text-770">Nhanh chóng và dễ dàng</p>
           </div>
           <div className="p-4">
             <div className="flex justify-between items-center w-full">
-              <div className="relative w-1/2">
+              <div className="relative w-1/2 mr-2">
                 <input
-                  className=" outline-none text-15px leading-4 border p-11px rounded"
+                  placeholder="Họ"
+                  className={`outline-none text-15px leading-4 p-11px rounded w-full ${
+                    errors.first_name ? 'border-red-500 border' : 'border'
+                  }`}
                   {...register('first_name', { required: true, maxLength: 8, minLength: 4 })}
                 />
-                <FontAwesomeIcon className="absolute top-1/2 -translate-y-1/2 right-3 text-red-600" icon={faCircleExclamation} />
+                {errors.first_name && (
+                  <FontAwesomeIcon className="absolute top-1/2 -translate-y-1/2 right-3 text-red-600" icon={faCircleExclamation} />
+                )}
               </div>
               <div className="relative w-1/2">
                 <input
-                  className=" outline-none text-15px leading-4 border p-11px rounded"
+                  placeholder="Tên"
+                  className={`outline-none text-15px leading-4 p-11px rounded w-full ${
+                    errors.last_name ? 'border-red-500 border' : 'border'
+                  }`}
                   {...register('last_name', { required: true, maxLength: 8, minLength: 4 })}
                 />
-                <FontAwesomeIcon className="absolute top-1/2 -translate-y-1/2 right-3 text-red-600" icon={faCircleExclamation} />
+                {errors.last_name && (
+                  <FontAwesomeIcon className="absolute top-1/2 -translate-y-1/2 right-3 text-red-600" icon={faCircleExclamation} />
+                )}
               </div>
             </div>
             <div className="relative my-4">
-              <input className="outline-none text-15px leading-4 border p-11px rounded w-full" />
-              <FontAwesomeIcon className="absolute top-1/2 -translate-y-1/2 right-3 text-red-600" icon={faCircleExclamation} />
+              <input
+                placeholder="Số di động hoặc email"
+                className={`outline-none text-15px leading-4 p-11px rounded w-full ${errors.username ? 'border-red-500 border' : 'border'}`}
+                {...register('username', { required: true, maxLength: 12, minLength: 4 })}
+              />
+              {errors.username && (
+                <FontAwesomeIcon className="absolute top-1/2 -translate-y-1/2 right-3 text-red-600" icon={faCircleExclamation} />
+              )}
             </div>
             <div className="relative">
-              <input className="outline-none text-15px leading-4 border p-11px rounded w-full" />
-              <FontAwesomeIcon className="absolute top-1/2 -translate-y-1/2 right-3 text-red-600" icon={faCircleExclamation} />
+              <input
+                type="password"
+                placeholder="Mật khẩu mới"
+                className={`outline-none text-15px leading-4 p-11px rounded w-full ${errors.password ? 'border-red-500 border' : 'border'}`}
+                {...register('password', { required: true, maxLength: 12, minLength: 4 })}
+              />
+              {errors.password && (
+                <FontAwesomeIcon className="absolute top-1/2 -translate-y-1/2 right-3 text-red-600" icon={faCircleExclamation} />
+              )}
             </div>
             <div className="flex flex-col mt-3">
               <p className="mb-1 text-770 text-xs">
@@ -77,25 +115,31 @@ const Register: React.FC = () => {
               </p>
               <div className="flex justify-around">
                 <select
+                  {...register('day')}
                   className="w-1/3 outline-none text-15px leading-4 border p-10px rounded"
-                  value={value.day}
-                  onChange={(e) => setValue({ ...value, day: +e.target.value })}>
+                  // value={value.day}
+                  // onChange={(e) => setValue({ ...value, day: +e.target.value })}
+                >
                   {day.map((a) => (
                     <option key={a}>{a}</option>
                   ))}
                 </select>
                 <select
+                  {...register('month')}
                   className="w-1/3 mx-2 outline-none text-15px leading-4 border p-10px rounded"
-                  value={value.month}
-                  onChange={(e) => setValue({ ...value, month: +e.target.value })}>
+                  // value={value.month}
+                  // onChange={(e) => setValue({ ...value, month: +e.target.value })}
+                >
                   {month.map((a) => (
                     <option key={a}>{a}</option>
                   ))}
                 </select>
                 <select
+                  {...register('year')}
                   className="w-1/3 outline-none text-15px leading-4 border p-10px rounded"
-                  value={value.year}
-                  onChange={(e) => setValue({ ...value, year: +e.target.value })}>
+                  // value={value.year}
+                  // onChange={(e) => setValue({ ...value, year: +e.target.value })}
+                >
                   {year.map((a) => (
                     <option key={a}>{a}</option>
                   ))}
@@ -107,19 +151,24 @@ const Register: React.FC = () => {
                 Giới tính <FontAwesomeIcon icon={faCircleQuestion} />
               </p>
               <div className="flex justify-around">
-                <div className='w-1/3 border rounded p-2 flex justify-between items-center'>
-                  <span className='text-15px'>Nữ</span>
-                  <input type="radio" />
+                <div className="w-1/3 border rounded p-2 flex justify-between items-center">
+                  <span className="text-15px">Nữ</span>
+                  <input {...register('gen')} type="radio" />
                 </div>
-                <div className='w-1/3 border rounded p-2 mx-2 flex justify-between items-center'>
-                  <span className='text-15px'>Nam</span>
-                  <input type="radio" />
+                <div className="w-1/3 border rounded p-2 mx-2 flex justify-between items-center">
+                  <span className="text-15px">Nam</span>
+                  <input {...register('gen')} type="radio" />
                 </div>
-                <div className='w-1/3 border rounded p-2 flex justify-between items-center'>
-                  <span className='text-15px'>Tùy chỉnh</span>
-                  <input type="radio" />
+                <div className="w-1/3 border rounded p-2 flex justify-between items-center">
+                  <span className="text-15px">Tùy chỉnh</span>
+                  <input {...register('gen')} type="radio" />
                 </div>
               </div>
+            </div>
+            <div className="w-full flex justify-center items-center mt-8 mb-5">
+              <button type="submit" className="w-1/3 text-white bg-green-600 rounded-md h-9 text-lg px-8 font-semibold">
+                Đăng ký
+              </button>
             </div>
           </div>
         </form>
